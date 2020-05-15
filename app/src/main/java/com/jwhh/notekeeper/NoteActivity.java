93eaad091233c61703887cd3d9e8a5a8970ffa6d
LennyDennis <lennydennis@gmail.com>
2020-05-15 15:39:10 +0300

    - Add an adapter to display the courses on the spinner

--- app/src/main/java/com/jwhh/notekeeper/NoteActivity.java
+++ app/src/main/java/com/jwhh/notekeeper/NoteActivity.java
@@ -11,7 +11,10 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+import java.util.List;
 
 public class NoteActivity extends AppCompatActivity {
 
@@ -24,6 +27,10 @@ public class NoteActivity extends AppCompatActivity {
 
         Spinner spinnerCourses = findViewById(R.id.spinner_courses);
 
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        ArrayAdapter<CourseInfo> courseInfoArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,courses);
+        courseInfoArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCourses.setAdapter(courseInfoArrayAdapter);
     }
 
     @Override
