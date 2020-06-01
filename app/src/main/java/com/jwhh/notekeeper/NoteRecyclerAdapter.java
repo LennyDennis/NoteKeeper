51ca3da383431fdfa9f258f54cbf79f9e6f8a611
LennyDennis <lennydennis@gmail.com>
2020-06-01 10:36:04 +0300

    - Add a RecyclerView to replace the list view - Add a cardview to display the course and note title - Add a recyclerViewAdapter to populate the data to the recyclerview

---
+++ app/src/main/java/com/jwhh/notekeeper/NoteRecyclerAdapter.java
@@ -0,0 +1,59 @@
+package com.jwhh.notekeeper;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>{
+
+    private final Context mContext;
+    private final List<NoteInfo> mNoteInfo;
+    private final LayoutInflater mLayoutInflater;
+
+    public NoteRecyclerAdapter(Context context, List<NoteInfo> noteInfo) {
+        mContext = context;
+        mNoteInfo = noteInfo;
+        mLayoutInflater = LayoutInflater.from(mContext);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public final TextView mTextCourse;
+        public final TextView mTextTitle;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mTextCourse = itemView.findViewById(R.id.text_course);
+            mTextTitle = itemView.findViewById(R.id.text_title);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemVIew = mLayoutInflater.inflate(R.layout.item_note_list, parent, false);
+        return new ViewHolder(itemVIew);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        NoteInfo noteInfo = mNoteInfo.get(position);
+        holder.mTextCourse.setText(noteInfo.getCourse().getTitle());
+        holder.mTextTitle.setText(noteInfo.getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNoteInfo.size();
+    }
+
+
+
+}
