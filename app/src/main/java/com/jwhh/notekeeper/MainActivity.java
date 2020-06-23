963578fbd30c786cc57c5a14ef3eb8d2e6eba53d
LennyDennis <lennydennis@gmail.com>
2020-06-23 09:43:22 +0300

    Add a setting activity to the project

--- app/src/main/java/com/jwhh/notekeeper/MainActivity.java
+++ app/src/main/java/com/jwhh/notekeeper/MainActivity.java
@@ -1,10 +1,13 @@
 package com.jwhh.notekeeper;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
@@ -22,6 +25,8 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -76,6 +81,22 @@ public class MainActivity extends AppCompatActivity
     protected void onResume() {
         super.onResume();
         mNoteRecyclerAdapter.notifyDataSetChanged();
+        updateNavHeader();
+    }
+
+    private void updateNavHeader() {
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        TextView userTextName = headerView.findViewById(R.id.text_user_name);
+        TextView userTextEmail = headerView.findViewById(R.id.text_email_address);
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String userName = sharedPreferences.getString("user_display_name","");
+        String userEmail = sharedPreferences.getString("user_email_address","");
+
+        userTextName.setText(userName);
+        userTextEmail.setText(userEmail);
+
     }
 
     private void initializeDisplayContent() {
@@ -149,7 +170,7 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_courses) {
             displayCourses();
         } else if (id == R.id.nav_share) {
-            handleSelection(R.string.nav_share_message);
+            handleShare();
         } else if (id == R.id.nav_send) {
             handleSelection(R.string.nav_send_message);
         }
@@ -159,10 +180,27 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    private void handleShare() {
+        View view = findViewById(R.id.list_items);
+        Snackbar.make(view, "Share to : "+
+                PreferenceManager.getDefaultSharedPreferences(this).getString("user_favorite_social",""),
+                Snackbar.LENGTH_LONG).show();
+
+    }
+
     private void handleSelection(int message_id) {
         View view = findViewById(R.id.list_items);
         Snackbar.make(view, message_id, Snackbar.LENGTH_LONG).show();
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id= item.getItemId();
 
+        if(id == R.id.action_settings){
+            startActivity(new Intent(this,SettingsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
